3eb6fe6728ff4e45b894d3e2fde8ac83e031efab
Andrew Skelton <andrew.j.skelton73@gmail.com>
2016-07-05 13:19:13 +0100

    Organised Each Exome Run
    Sorted each exome run by batch, altered directory structure, and
    organised by capture kit.

--- Core/Core_Preprocess.sh
+++ Core/Core_Preprocess.sh
@@ -14,41 +14,39 @@ PROJ_BASE="/home/nas151/WORKING_DATA/Exome_Project"
 SCRIPTS=${PROJ_BASE}/Scripts
 BUNDLE="/opt/databases/GATK_bundle/2.8/hg19"
 
-##'Older Samples - Still to be organised
+##' Older Samples - Still to be organised
 # BASE_DIR="/home/nas151/WORKING_DATA/Exomes/old_samples/Louise"
 # BASE_DIR="/home/nas151/WORKING_DATA/Exomes/old_samples/Rafi"
 # CAP_KIT="/home/nas151/Agilent_SureSelect_ExomeV1/S0274956_Covered.bed" #SureSelect V1
 # CAP_KIT="/home/nas151/Agilent_SureSelect_Exome50MB/S04380110_Covered.bed" #SureSelect 50MB
-#BASE_DIR=${PROJ_BASE}/SamplePreprocessing/Old_Samples
-#BATCH="Old_Samples"
-#CAP_KIT=${PROJ_BASE}/Capture_Kits/Agilent_SureSelect_ExomeV5/S04380110_Covered.bed
+# BASE_DIR=${PROJ_BASE}/SamplePreprocessing/Old_Samples
+# BATCH="Old_Samples"
+# CAP_KIT=${PROJ_BASE}/Capture_Kits/Agilent_SureSelect_ExomeV5/S04380110_Covered.bed
 
-##'AROS Batches
-# BASE_DIR="/home/nas151/WORKING_DATA/Exome_Project/SamplePreprocessing/A2463/Batch_113_1"
-# BATCH="A2463_Batch_113_1"
-# BASE_DIR="${PROJ_BASE}/SamplePreprocessing/A2463/Batch_116_1"
-# BATCH="A2463_Batch_116_1"
-# BASE_DIR="${PROJ_BASE}/SamplePreprocessing/APR16"
-# BATCH="APR16"
-# BASE_DIR="${PROJ_BASE}/SamplePreprocessing/MAY16/Batch_1"
-# BATCH="MAY16_1"
-#Illumina Nextera
-# BASE_DIR="/home/nas151/WORKING_DATA/Exome_Project/Preprocessing/2016/June/"
-# BATCH="B2016May"
-# CAP_KIT=${PROJ_BASE}/Capture_Kits/Nextera_Rapid_Capture_Exome/nexterarapidcapture_exome_targetedregions.bed
-BASE_DIR="/home/nas151/WORKING_DATA/Exome_Project/Preprocessing/2015/December/"
-BATCH="B2015December"
+
+# BASE_DIR="/home/nas151/WORKING_DATA/Exome_Project/Preprocessing/2015/December/"
+# BATCH="B2015December"
+BASE_DIR="/home/nas151/WORKING_DATA/Exome_Project/Preprocessing/2013/May/"
+BATCH="B2013May_A1969_1"
+# BASE_DIR="/home/nas151/WORKING_DATA/Exome_Project/Preprocessing/2013/December/"
+# BATCH="B2013December_A1969_2"
+# BASE_DIR="/home/nas151/WORKING_DATA/Exome_Project/Preprocessing/2014/July/"
+# BATCH="B2014July_A1969_3"
 CAP_KIT=${PROJ_BASE}/Capture_Kits/Agilent_SureSelect_ExomeV5/S04380110_Covered.bed
+#
+#
+# BASE_DIR="/home/nas151/WORKING_DATA/Exome_Project/Preprocessing/2014/September1/"
+# BATCH="B2014September1_A2463"
+# BASE_DIR="/home/nas151/WORKING_DATA/Exome_Project/Preprocessing/2014/September2/"
+# BATCH="B2014September2_A2463"
+# BASE_DIR="/home/nas151/WORKING_DATA/Exome_Project/Preprocessing/2016/April/"
+# BATCH="B2016April"
+# BASE_DIR="/home/nas151/WORKING_DATA/Exome_Project/Preprocessing/2016/May/"
+# BATCH="B2016May"
+# BASE_DIR="/home/nas151/WORKING_DATA/Exome_Project/Preprocessing/2016/June/"
+# BATCH="B2016June"
+# CAP_KIT=${PROJ_BASE}/Capture_Kits/Nextera_Rapid_Capture_Exome/nexterarapidcapture_exome_targetedregions.bed
 
-# BASE_DIR="${PROJ_BASE}/SamplePreprocessing/A1969/Batch_1"
-# BATCH="A1969_Batch_1"
-# BASE_DIR="${PROJ_BASE}/SamplePreprocessing/A1969/Batch_2"
-# BATCH="A1969_Batch_2"
-# BASE_DIR="${PROJ_BASE}/SamplePreprocessing/A1969/Batch_3"
-# BATCH="A1969_Batch_3"
-# BASE_DIR=${PROJ_BASE}/SamplePreprocessing/DEC15
-# BATCH="DEC15"
-#SureSelect V5
 
 ##'-----------------------------------------------------------------------------------------#
 
@@ -78,7 +76,9 @@ FORWARD_READS=`ls $i'/Raw_Data/'*R1*`
 FORWARD_READS=$(basename "$FORWARD_READS")
 REVERSE_READS=`ls $i'/Raw_Data/'*R2*`
 REVERSE_READS=$(basename "$REVERSE_READS")
-RG='@RG\tID:'${BATCH}'\tSM:'${SAMPLE_ID}'\tPL:Illumina\tLB:Nextera\tPU:NextSeq'
+# Nextera  NextSeq
+# Agilent_SureSelect_ExomeV5
+RG='@RG\tID:'${BATCH}'\tSM:'${SAMPLE_ID}'\tPL:Illumina\tLB:Agilent_SureSelect_ExomeV5\tPU:HISeq'
 PADDING_TAR="${BASE_DIR}/Sample_${SAMPLE_ID}/Raw_Data/${SAMPLE_ID}_R1.fastq.gz"
 PADDING=$(zcat ${PADDING_TAR} | head -10000 | awk '{print length}' | sort -nr | head -1)
 ##'-----------------------------------------------------------------------------------------#
@@ -123,14 +123,14 @@ if ! ls ${BASE_DIR}/Sample_${SAMPLE_ID}/Alignment/Clean/${SAMPLE_ID}_Clean_GATK.
     ##' $4 - Path to Paired Fastq Files
     ##' $5 - Path to Sample's preprocessing base
     ##'---------------------------------------------------------------------------------------#
-    # qsub -N "BWA_P_${SAMPLE_ID}" \
-    #         -hold_jid "FastqSort_${SAMPLE_ID}" \
-    #           ${SCRIPTS}/Modules/Module_BWA_MEM_P.sh \
-    #           ${RG} \
-    #           ${REF_FA} \
-    #           ${SAMPLE_ID} \
-    #           ${i}/Raw_Data \
-    #           ${i}
+    qsub -N "BWA_P_${SAMPLE_ID}" \
+            -hold_jid "FastqSort_${SAMPLE_ID}" \
+              ${SCRIPTS}/Modules/Module_BWA_MEM_P.sh \
+              ${RG} \
+              ${REF_FA} \
+              ${SAMPLE_ID} \
+              ${i}/Raw_Data \
+              ${i}
     ##'---------------------------------------------------------------------------------------#
 
 
@@ -138,11 +138,11 @@ if ! ls ${BASE_DIR}/Sample_${SAMPLE_ID}/Alignment/Clean/${SAMPLE_ID}_Clean_GATK.
     ##' $1 - Sample ID
     ##' $2 - Path to Sample's preprocessing base
     ##'---------------------------------------------------------------------------------------#
-    # qsub -N "Picard_${SAMPLE_ID}" \
-    #         -hold_jid "BWA_*_${SAMPLE_ID}" \
-    #           ${SCRIPTS}/Modules/Module_PicardNT.sh \
-    #           ${SAMPLE_ID} \
-    #           ${i}
+    qsub -N "Picard_${SAMPLE_ID}" \
+            -hold_jid "BWA_*_${SAMPLE_ID}" \
+              ${SCRIPTS}/Modules/Module_PicardNT.sh \
+              ${SAMPLE_ID} \
+              ${i}
     ##'---------------------------------------------------------------------------------------#
 
 
@@ -150,18 +150,18 @@ if ! ls ${BASE_DIR}/Sample_${SAMPLE_ID}/Alignment/Clean/${SAMPLE_ID}_Clean_GATK.
     ##' $1 - Sample ID
     ##' $2 - Path to Sample's preprocessing base
     ##'---------------------------------------------------------------------------------------#
-    # qsub -N "GATKRecal_${SAMPLE_ID}" \
-    #         -hold_jid "Picard_${SAMPLE_ID}" \
-    #           ${SCRIPTS}/Modules/Module_GATKRecal.sh \
-    #           ${SAMPLE_ID} \
-    #           ${i} \
-    #           ${REF_FA} \
-    #           ${MILLS} \
-    #           ${PHASE1INDELS} \
-    #           ${DBSNP} \
-    #           ${CAP_KIT} \
-    #           ${BUNDLE} \
-    #           ${PADDING}
+    qsub -N "GATKRecal_${SAMPLE_ID}" \
+            -hold_jid "Picard_${SAMPLE_ID}" \
+              ${SCRIPTS}/Modules/Module_GATKRecal.sh \
+              ${SAMPLE_ID} \
+              ${i} \
+              ${REF_FA} \
+              ${MILLS} \
+              ${PHASE1INDELS} \
+              ${DBSNP} \
+              ${CAP_KIT} \
+              ${BUNDLE} \
+              ${PADDING}
     ##'---------------------------------------------------------------------------------------#
 
 ##'---------------------------------------------------------------------------------------#
@@ -173,18 +173,18 @@ fi
 ##' $1 - Sample ID
 ##' $2 - Path to Sample's preprocessing base
 ##'---------------------------------------------------------------------------------------#
-# if ! ls ${i}/GATK/${SAMPLE_ID}.g.vcf 1> /dev/null 2>&1; then
-#   qsub -N "GATKgVCF_${SAMPLE_ID}" \
-#           -hold_jid "GATKRecal_${SAMPLE_ID}" \
-#             ${SCRIPTS}/Modules/Module_GATKgVCF.sh \
-#             ${SAMPLE_ID} \
-#             ${i} \
-#             ${REF_FA} \
-#             ${BUNDLE} \
-#             ${CAP_KIT} \
-#             ${DBSNP} \
-#             ${PADDING}
-# fi
+if ! ls ${i}/GATK/${SAMPLE_ID}.g.vcf 1> /dev/null 2>&1; then
+  qsub -N "GATKgVCF_${SAMPLE_ID}" \
+          -hold_jid "GATKRecal_${SAMPLE_ID}" \
+            ${SCRIPTS}/Modules/Module_GATKgVCF.sh \
+            ${SAMPLE_ID} \
+            ${i} \
+            ${REF_FA} \
+            ${BUNDLE} \
+            ${CAP_KIT} \
+            ${DBSNP} \
+            ${PADDING}
+fi
 ##'---------------------------------------------------------------------------------------#
 
 
