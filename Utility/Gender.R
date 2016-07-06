0033570d4d07bb12a368c1562fec863b12202877
Andrew Skelton <andrew.j.skelton73@gmail.com>
2016-07-06 16:02:34 +0100

    New Input
    Altered to deal with new input type (SampleMap.txt)

--- Utility/Gender.R
+++ Utility/Gender.R
@@ -16,7 +16,7 @@ library(dplyr)
 
 setwd("/Volumes/WORKING_DATA/Exome_Project/")
 
-ped_in       <- read_tsv("Scripts/Ref/SampleMap.txt", col_names = F) %>% 
+ped_in       <- read_tsv("Scripts/Ref/SampleMap_.txt", col_names = T) %>% 
                 as.data.frame %>% 
                 mutate(Gender_Prediction = "")
 ##'-----------------------------------------------------------------------------------------#
@@ -32,12 +32,14 @@ cov_files     <- list.files("Preprocessing/",
                             recursive  = T)
 cov_names     <- sapply(cov_files, function(x){strsplit(basename(x),"_")[[1]][1]})
 for(i in 1:length(cov_files)){
-  message(cov_files[i])
-  message(file.size(cov_files[i]))
-  foo      <- read_tsv(cov_files[i], col_names = F) %>%
-              as.data.frame %>%
-              mutate(Sample = cov_names[i])
-  coverage <- bind_rows(coverage, foo)
+  if(file.size(cov_files[i]) > 0) {
+    foo      <- read_tsv(cov_files[i], col_names = F) %>%
+                as.data.frame %>%
+                mutate(Sample = cov_names[i])
+    coverage <- bind_rows(coverage, foo)
+  } else {
+    message(paste0("Empty File: ", cov_files[i]))
+  }
 }
 ##'-----------------------------------------------------------------------------------------#
 
@@ -59,19 +61,20 @@ sample_error <- c()
 error_out    <- c()
 for(i in unique(coverage$Sample)){
   foo <- coverage %>% filter(Sample == i) %>% 
-         left_join(ped_in, by = c("Sample" = "X2"))
-  foo_known <- foo[["X5.y"]][1]
-  foo_Pred  <- ifelse(mean(foo$X6.x) > 5, "M", "F")
-  # message(paste0(foo_known, " ", foo_Pred, " ", mean(foo$X6.x)))
+         left_join(ped_in, by = c("Sample" = "Sample_ID"))
+  foo_known <- foo[["Gender"]][1]
+  foo_Pred  <- ifelse(mean(foo$X6) > 5, "M", "F")
+  # message(paste0(foo_known, " ", foo_Pred, " ", mean(foo$X6)))
+  # message(i)
   if(foo_known != foo_Pred){
-    message(paste0("Potential Sample Mismatch: ", i, 
+    message(paste0("Potential Sample Mismatch: ", i,
                    "\n  Logged as ", foo_known,
                    ", SRY coverage suggests ",
-                   foo_Pred, "\n  Mean Cov: ", mean(foo$X6.x), "\n"))
+                   foo_Pred, "\n  Mean Cov: ", mean(foo$X6), "\n"))
     sample_error <- c(sample_error,i)
     error_out    <- bind_rows(error_out,
                               data.frame(SampleID = i,
-                                         MeanSRY  = mean(foo$X6.x) %>% round(2),
+                                         MeanSRY  = mean(foo$X6) %>% round(2),
                                          Logged_Gender = foo_known,
                                          Predicted_Gender = foo_Pred))
   }
@@ -84,13 +87,14 @@ for(i in unique(coverage$Sample)){
 ##'-----------------------------------------------------------------------------------------#
 coverage_in <- coverage %>% 
                filter(Sample %in% sample_error) %>% 
-               left_join(ped_in, by = c("Sample" = "X2"))
-gg <- ggplot(coverage_in, aes(x = X5.x, y = X6.x, group = Sample, colour = X5.y)) +
+               left_join(ped_in, by = c("Sample" = "Sample_ID"))
+gg <- ggplot(coverage_in, aes(x = X5, y = X6, group = Sample, colour = Gender)) +
       geom_line(size = 1) +
       facet_grid(Sample ~ .) +
       xlab("Locus") +
       ylab("Depth") +
       theme_bw()
+print(gg)
 ##'-----------------------------------------------------------------------------------------#
 
 
